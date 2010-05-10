c71f6c2d31410d9624fe3225a52be92968428dce
takuo <kitame@gmail.com>
2010-05-11 06:48:54 +0900

    fix layout

--- gen/jp/takuo/android/mmsreq/R.java
+++ gen/jp/takuo/android/mmsreq/R.java
@@ -19,14 +19,11 @@ public final class R {
     public static final class id {
         public static final int RelativeLayout01=0x7f060000;
         public static final int TableLayout01=0x7f060001;
-        public static final int TableRow01=0x7f060004;
-        public static final int TableRow02=0x7f060006;
-        public static final int TableRow03=0x7f060008;
-        public static final int TableRow04=0x7f060002;
-        public static final int TextView01=0x7f060005;
-        public static final int b_request=0x7f060007;
-        public static final int spinner_type=0x7f060003;
-        public static final int t_result=0x7f060009;
+        public static final int TableRow01=0x7f060003;
+        public static final int TextView01=0x7f060002;
+        public static final int b_request=0x7f060005;
+        public static final int spinner_type=0x7f060004;
+        public static final int t_result=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
